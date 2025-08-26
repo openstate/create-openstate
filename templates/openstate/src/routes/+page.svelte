920ce8f46d829fe9c2720ea194742c92076f58de
Sicco van Sas <siccovansas@users.noreply.github.com>
2025-08-26 13:32:39 +0200

    Update +page.svelte

--- templates/openstate/src/routes/+page.svelte
+++ templates/openstate/src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <h1>Welcome to ~DESCRIPTION~</h1>
-<p class="lead">This project was generated using the <code>openspending-create</code> package.</p>
+<p class="lead">This project was generated using the <code>create-openstate</code> package.</p>
 <p>To edit this page, open your favourite editor/IDE and edit this page: <code>`src/routes/+page.svelte`</code></p>
